7ee25bb7367fea86d2947d4f4c3282bc01df30b9
Zihong Hao <haozihong@hotmail.com>
2022-11-23 15:11:44 -0500

    add ingredient controller

---
+++ rse-api/src/main/java/edu/gatech/cs4400/fall22/team119/rse/controller/IngredientController.java
@@ -0,0 +1,28 @@
+package edu.gatech.cs4400.fall22.team119.rse.controller;
+
+import edu.gatech.cs4400.fall22.team119.rse.pojo.Ingredient;
+import edu.gatech.cs4400.fall22.team119.rse.service.IngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author Zihong Hao
+ * @date 2022/11/23 14:48
+ */
+@RestController
+public class IngredientController {
+    private IngredientService ingredientService;
+
+    @Autowired
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @GetMapping("/ingredients")
+    public List<Ingredient> getAllIngredients() {
+        return ingredientService.getAllIngredients();
+    }
+}
