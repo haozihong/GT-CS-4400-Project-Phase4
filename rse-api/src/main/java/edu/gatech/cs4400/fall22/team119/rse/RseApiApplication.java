28d818d7d29202a082e34cb01c048cc08fba0905
Zihong Hao <haozihong@hotmail.com>
2022-11-23 15:10:34 -0500

    add main entry and configuration files

---
+++ rse-api/src/main/java/edu/gatech/cs4400/fall22/team119/rse/RseApiApplication.java
@@ -0,0 +1,15 @@
+package edu.gatech.cs4400.fall22.team119.rse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zihong Hao
+ * @date 2022/11/23 14:07
+ */
+@SpringBootApplication
+public class RseApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RseApiApplication.class, args);
+    }
+}
