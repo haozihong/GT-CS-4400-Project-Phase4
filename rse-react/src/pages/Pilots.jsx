f52fe61808717b150c1ce661b9cf354ac168ebd9
Zihong Hao <haozihong@hotmail.com>
2022-12-03 00:29:05 -0500

    [frontend] init pilots page

--- rse-react/src/pages/Pilots.jsx
+++ rse-react/src/pages/Pilots.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Table } from 'antd';
 
-const Pilots = () => {
+const columns = [
+  { title: 'Username', dataIndex: 'username' },
+  { title: 'License ID', dataIndex: 'licenseID' },
+  { title: 'Piloting Experience (trips)', dataIndex: 'experience' },
+  { title: 'Num of Drones', dataIndex: 'numDrones' },
+  { title: 'Num of Locations', dataIndex: 'numLocations' },
+];
+
+export const Pilots = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const fetchData = () => {
+    setLoading(true);
+    fetch(`/api/pilots`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div>Pilots</div>
-  )
+    <Table
+      columns={columns}
+      rowKey={(record) => record.username}
+      dataSource={data}
+      loading={loading}
+    />
+  );
 }
 
 export default Pilots
