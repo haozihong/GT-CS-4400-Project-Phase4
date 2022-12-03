22ab76db900017702a483a91b70e2df22829c227
Zihong Hao <haozihong@hotmail.com>
2022-12-03 00:29:04 -0500

    install ant design

--- rse-react/src/App.js
+++ rse-react/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
 import './App.css';
 
 const App = () => (
-    <div className="App">App</div>
+    <div className="App">
+        <Button type="primary">Button</Button>
+    </div>
 );
 
 export default App;
