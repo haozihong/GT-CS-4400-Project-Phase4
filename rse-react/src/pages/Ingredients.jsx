ac1c70ea4f28f798fac83e590a0147b96c9f1f71
Zihong Hao <haozihong@hotmail.com>
2022-12-03 00:29:05 -0500

    [frontend] init ingredients page

--- rse-react/src/pages/Ingredients.jsx
+++ rse-react/src/pages/Ingredients.jsx
@@ -1,86 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { Table } from 'antd';
-import qs from 'qs';
+
 const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    sorter: true,
-    render: (name) => `${name.first} ${name.last}`,
-    width: '20%',
-  },
-  {
-    title: 'Gender',
-    dataIndex: 'gender',
-    filters: [
-      {
-        text: 'Male',
-        value: 'male',
-      },
-      {
-        text: 'Female',
-        value: 'female',
-      },
-    ],
-    width: '20%',
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-  },
+  { title: 'Ingredient Name', dataIndex: 'iname' },
+  { title: 'Location', dataIndex: 'location' },
+  { title: 'Amount Available', dataIndex: 'amount_available' },
+  { title: 'Low Price', dataIndex: 'low_price' },
+  { title: 'High Price', dataIndex: 'high_price' },
 ];
-const getRandomuserParams = (params) => ({
-  results: params.pagination?.pageSize,
-  page: params.pagination?.current,
-  ...params,
-});
+
 const App = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(false);
-  const [tableParams, setTableParams] = useState({
-    pagination: {
-      current: 1,
-      pageSize: 10,
-    },
-  });
   const fetchData = () => {
     setLoading(true);
-    fetch(`https://randomuser.me/api?${qs.stringify(getRandomuserParams(tableParams))}`)
+    fetch(`/api/ingredients`)
       .then((res) => res.json())
-      .then(({ results }) => {
-        setData(results);
+      .then((res) => {
+        setData(res);
         setLoading(false);
-        setTableParams({
-          ...tableParams,
-          pagination: {
-            ...tableParams.pagination,
-            total: 200,
-            // 200 is mock data, you should read it from server
-            // total: data.totalCount,
-          },
-        });
       });
   };
 
   useEffect(() => {
     fetchData();
-  }, [JSON.stringify(tableParams)]);
-  const handleTableChange = (pagination, filters, sorter) => {
-    setTableParams({
-      pagination,
-      filters,
-      ...sorter,
-    });
-  };
+  }, []);
   return (
     <Table
       columns={columns}
-      rowKey={(record) => record.login.uuid}
+      rowKey={(record) => record.barcode}
       dataSource={data}
-      pagination={tableParams.pagination}
       loading={loading}
-      onChange={handleTableChange}
     />
   );
 };
+
 export default App;
