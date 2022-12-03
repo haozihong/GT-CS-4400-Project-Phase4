25b0c3dc3deb94d4e8b69276437462fcbac2527b
Zihong Hao <haozihong@hotmail.com>
2022-12-03 00:29:05 -0500

    update with recommened router setting

--- rse-react/src/router/index.jsx
+++ rse-react/src/router/index.jsx
@@ -1,16 +1,20 @@
 import App from "../App";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import routes from './routes'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "u", element: <Navigate to='/' />, },
+      ...routes.map(({ _, url, page }) => ({path: url, element: page}))
+    ],
+  },
+]);
+
 const BaseRouter = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/u" element={ <Navigate to='/' /> } />
-        { routes.map(({ _, url, page }) => <Route path={url} element={page} />) }
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <RouterProvider router={router} />
 );
 
 export default BaseRouter;
