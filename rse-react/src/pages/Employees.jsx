2e296b1e49b6c49d1677ed329403f20b1327e143
Zihong Hao <haozihong@hotmail.com>
2022-12-03 00:29:05 -0500

    [frontend] init employees page

--- rse-react/src/pages/Employees.jsx
+++ rse-react/src/pages/Employees.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Table } from 'antd';
+
+const columns = [
+  { title: 'Username', dataIndex: 'username' },
+  { title: 'Tax ID', dataIndex: 'taxID' },
+  { title: 'Salary ($)', dataIndex: 'salary' },
+  { title: 'Hired Datw', dataIndex: 'hired' },
+  { title: 'Experience (month)', dataIndex: 'employeeExperience' },
+  { title: 'License ID', dataIndex: 'licenseID' },
+  { title: 'Piloting Experience (trips)', dataIndex: 'pilotingExperience' },
+  { title: 'Manager Status', dataIndex: 'managerStatus' },
+];
 
 export const Employees = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const fetchData = () => {
+    setLoading(true);
+    fetch(`/api/employees`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div>Employees</div>
-  )
+    <Table
+      columns={columns}
+      rowKey={(record) => record.username}
+      dataSource={data}
+      loading={loading}
+    />
+  );
 }
 
 export default Employees
