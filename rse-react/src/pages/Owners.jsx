97a9d3595a27caa58a6160f0a36d8c4a0b1c98eb
Zihong Hao <haozihong@hotmail.com>
2022-12-03 00:29:05 -0500

    [frontend] init owners page

--- rse-react/src/pages/Owners.jsx
+++ rse-react/src/pages/Owners.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Table } from 'antd';
 
-const Owners = () => {
+const columns = [
+  { title: 'Username', dataIndex: 'username' },
+  { title: 'First Name', dataIndex: 'firstName' },
+  { title: 'Last Name', dataIndex: 'lastName' },
+  { title: 'Address', dataIndex: 'address' },
+  { title: 'Num of Restaurants', dataIndex: 'numRestaurants' },
+  { title: 'Num of Places', dataIndex: 'numPlaces' },
+  { title: 'Highest Rating', dataIndex: 'highs' },
+  { title: 'Lowest Rating', dataIndex: 'lows' },
+  { title: 'Debt', dataIndex: 'debt' },
+];
+
+export const Owners = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const fetchData = () => {
+    setLoading(true);
+    fetch(`/api/owners`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div>Owners</div>
-  )
+    <Table
+      columns={columns}
+      rowKey={(record) => record.username}
+      dataSource={data}
+      loading={loading}
+    />
+  );
 }
 
 export default Owners
