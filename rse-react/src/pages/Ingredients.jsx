c831e4c721a8e4300016b4d4ac0c306f5c22cf22
Zihong Hao <haozihong@hotmail.com>
2022-12-11 23:59:43 -0500

    refactor ingredients page

--- rse-react/src/pages/Ingredients.jsx
+++ rse-react/src/pages/Ingredients.jsx
@@ -1,19 +1,7 @@
 import React, { useEffect, useState } from "react";
-import {
-  Table,
-  Space,
-  Button,
-  Col,
-  Row,
-  Modal,
-  Form,
-  Input,
-  InputNumber,
-  Select,
-  notification,
-  Tooltip,
-} from "antd";
+import { Table, Space, Button, Col, Row, Input, InputNumber, Select, notification, Tooltip, } from "antd";
 import { PlusOutlined, MinusOutlined, ReloadOutlined } from "@ant-design/icons";
+import { FormModal } from "../components/FormModal";
 
 //print table
 const columns = [
@@ -23,26 +11,6 @@ const columns = [
   { title: "Amount Available", dataIndex: "amountAvailable" },
   { title: "Low Price", dataIndex: "lowPrice" },
   { title: "High Price", dataIndex: "highPrice" },
-];
-
-// pop_form
-const newIngredFormFields = [
-  {
-    name: "barcode",
-    label: "Ingredient Barcode",
-    formItem: <Input />,
-  },
-  {
-    name: "iname",
-    label: "Ingredient Name",
-    formItem: <Input />,
-  },
-  {
-    name: "weight",
-    label: "Ingredient Weight",
-    formItem: <InputNumber />,
-    rules: [{ required: true }, { type: "number", min: 0 }],
-  },
 ];
 
 const App = () => {
@@ -76,12 +44,8 @@ const App = () => {
     fetchAllData();
   }, []);
 
-  //add_操作和variables:
-  const [newIngredDialogOpen, setNewIngredDialogOpen] = useState(false);
+  const [addIngredDialogOpen, setAddIngredDialogOpen] = useState(false);
   const [removeIngredDialogOpen, setRemoveIngredDialogOpen] = useState(false);
-  const [confirmLoading, setConfirmLoading] = useState(false);
-  const [formAdd] = Form.useForm();
-  const [formRemove] = Form.useForm();
   const [notificationApi, contextHolder] = notification.useNotification();
   const popMessage = (message, description, type) => {
     notificationApi[type || "open"]({
@@ -90,125 +54,50 @@ const App = () => {
     });
   };
 
-  const newIngredDialogOk = () => {
-    setConfirmLoading(true);
-    formAdd.submit();
-  };
-  //上传文件
-  const onFinishAdd = (values) => {
-    //通过api 请求后端相应
-    fetch("/api/ingredients", {
-      method: "POST",
-      body: JSON.stringify(values),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      //获取成功，请求一个json文件并将其打印到控制台，returns a promise that resolves with a Response object.
-      .then((response) => {
-        if (!response.ok) return response.json().then((r) => Promise.reject(r));
-        return response.json();
-      })
-      // 添加失败，提出警告
-      .then(
-        (data) => {
-          if (data === 0) {
-            popMessage(
-              "Fail to add ingredients",
-              "Please check the form fields. ",
-              "warning"
-            );
-          } else {
-            // 成功添加，给出成功信息
-            fetchAllData();
-            setNewIngredDialogOpen(false);
-            popMessage(
-              "Success",
-              `Your Ingredients was added seccessfully!`,
-              "success"
-            );
-            formAdd.resetFields();
-          }
-        },
-        (err) => {
-          console.log("err", err);
-          popMessage(
-            `Server error ${err.status}`,
-            `${err.error}${err.message}`,
-            "error"
-          );
-        }
-      )
-      .catch((err) => {
-        popMessage(
-          "Fetch Fail",
-          "There has been a problem with your fetch operation",
-          "error"
-        );
-      })
-      .finally(() => {
-        setConfirmLoading(false);
-      });
-  };
+  const addIngredFormFields = [
+    { name: 'barcode', label: 'Ingredient Barcode', formItem: <Input />, },
+    { name: "iname", label: "Ingredient Name", formItem: <Input />, },
+    {
+      name: "weight",
+      label: "Ingredient Weight",
+      formItem: <InputNumber />,
+      rules: [{ required: true }, { type: "number", min: 0 }],
+    },
+  ];
 
-  //remove_items
-  const removeIngredDialogOk = () => {
-    setConfirmLoading(true);
-    formRemove.submit();
-  };
+  const removeIngredFormFields = [
+    {
+      name: 'barcode',
+      label: 'Ingredient Barcode',
+      formItem: 
+        <Select 
+          placeholder='Select a ingredient'
+          options={ingredients.map(e => ({ label: e.barcode, value: e.barcode }))}
+        />,
+    },
+  ];
 
-  const onFinishRemove = (values) => {
-    //通过api 请求后端相应
-    fetch(`/api/ingredients/${values.barcode}`, {
-      method: "DELETE", //remove 应该对应get吗
-    })
-      //获取成功，请求一个json文件并将其打印到控制台，returns a promise that resolves with a Response object.
-      .then((response) => {
-        if (!response.ok) return response.json().then((r) => Promise.reject(r));
-        return response.json();
-      })
-      // 添加失败，提出警告
-      .then(
-        (data) => {
-          if (data === 0) {
-            // 大于原有的amount
-            popMessage(
-              "Fail to remove ingredients",
-              "Please check the form fields. ",
-              "warning"
-            );
-          } else {
-            // 成功添加，给出成功信息
-            fetchAllData();
-            setNewIngredDialogOpen(false);
-            popMessage(
-              "Success",
-              `Your Ingredients was removed seccessfully!`,
-              "success"
-            );
-            formRemove.resetFields();
-          }
-        },
-        (err) => {
-          console.log("err", err);
-          popMessage(
-            `Server error ${err.status}`,
-            `${err.error}${err.message}`,
-            "error"
-          );
-        }
-      )
-      .catch((err) => {
-        popMessage(
-          "Fetch Fail",
-          "There has been a problem with your fetch operation",
-          "error"
-        );
-      })
-      .finally(() => {
-        setConfirmLoading(false);
-      });
-  };
+  const addIngredFormFinishArgs = {
+    fetchConfig: values => ([
+      `/api/ingredients`, 
+      {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-type": "application/json; charset=UTF-8", },
+      }
+    ]),
+    succDecs: 'Your Ingredients was added seccessfully!',
+    failMsg: 'Fail to add ingredients',
+  }
+
+  const removeIngredFormFinishArgs = {
+    fetchConfig: values => ([
+      `/api/ingredients/${values.barcode}`, 
+      { method: "DELETE", }
+    ]),
+    succDecs: 'Your Ingredients was removed seccessfully!',
+    failMsg: 'Fail to remove ingredients',
+  }
 
   return (
     <>
@@ -216,21 +105,11 @@ const App = () => {
       <Row className="page-content" gutter={[16, 8]}>
         <Col span={24} style={{ paddingTop: 16 }}>
           <Space style={{ float: "right" }}>
-            {/*Add button*/}
-            <Button
-              type="primary"
-              icon={<PlusOutlined />}
-              onClick={() => setNewIngredDialogOpen(true)}
-            >
+            <Button type="primary" icon={<PlusOutlined />} onClick={() => setAddIngredDialogOpen(true)}>
               Add
             </Button>
 
-            {/*Remove button*/}
-            <Button
-              type="default"
-              icon={<MinusOutlined />}
-              onClick={() => setRemoveIngredDialogOpen(true)}
-            >
+            <Button icon={<MinusOutlined />} onClick={() => setRemoveIngredDialogOpen(true)}>
               Remove
             </Button>
 
@@ -256,69 +135,25 @@ const App = () => {
         </Col>
       </Row>
 
-      {/* add 需要的form ++ check box*/}
-      <Modal
-        title="New Ingredients"
-        okText="Add Ingredients"
-        open={newIngredDialogOpen}
-        onOk={newIngredDialogOk}
-        confirmLoading={confirmLoading}
-        onCancel={() => setNewIngredDialogOpen(false)}
-      >
-        <Form
-          form={formAdd}
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          requiredMark="optional"
-          name="ingreForm"
-          onFinish={onFinishAdd}
-          onFinishFailed={() => setConfirmLoading(false)}
-        >
-          {/* add 需要form */}
-          {newIngredFormFields.map((e) => (
-            <Form.Item
-              name={e.name}
-              label={e.label}
-              rules={e.rules || [{ required: true }]}
-            >
-              {e.formItem}
-            </Form.Item>
-          ))}
-        </Form>
-      </Modal>
+      <FormModal
+        dialogOpenState={[addIngredDialogOpen, setAddIngredDialogOpen]}
+        formFields={addIngredFormFields}
+        formFinishArgs={addIngredFormFinishArgs}
+        refreshFn={fetchAllData}
+        popMessage={popMessage}
+        title='New Ingredients'
+        okText='Add Ingredients'
+      />
 
-      {/* remove 需要的form */}
-
-      <Modal
-        title="Remove Ingredients"
-        okText="Delete Ingredients"
-        open={removeIngredDialogOpen}
-        onOk={removeIngredDialogOk}
-        confirmLoading={confirmLoading}
-        onCancel={() => setRemoveIngredDialogOpen(false)}
-      >
-        <Form
-          form={formRemove}
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          requiredMark="optional"
-          name="ingreForm"
-          onFinish={onFinishRemove}
-          onFinishFailed={() => setConfirmLoading(false)}
-        >
-          {/* showing the pop_up_form */}
-          <Form.Item
-            name="barcode"
-            label="Ingredient Barcode"
-            rules={[{ required: true }]}
-          >
-            <Select 
-              placeholder='Select a ingredient'
-              options={ingredients.map(e => ({ label: e.barcode, value: e.barcode }))}
-            />
-          </Form.Item>
-        </Form>
-      </Modal>
+      <FormModal
+        dialogOpenState={[removeIngredDialogOpen, setRemoveIngredDialogOpen]}
+        formFields={removeIngredFormFields}
+        formFinishArgs={removeIngredFormFinishArgs}
+        refreshFn={fetchAllData}
+        popMessage={popMessage}
+        title='Remove Ingredients'
+        okText='Remove Ingredients'
+      />
     </>
   );
 };
